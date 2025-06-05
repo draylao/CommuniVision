520a364d35f3279833d79cca8c3405a8cbf9eb29
Dray <dray.lao@gmail.com>
2025-06-05 14:12:10 +0800

    Main

---
+++ src/main/java/com/o4/CommuniVision/CommuniVisionApplication.java
@@ -0,0 +1,13 @@
+package com.o4.CommuniVision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuniVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuniVisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/o4/CommuniVision/CommuniVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.o4.CommuniVision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuniVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
