3bab18d3dfb24f1f5643bf12ada1984d3e3f09ad
Dray <dray.lao@gmail.com>
2025-06-05 14:33:49 +0800

    test

--- src/main/java/com/o4/CommuniVision/CommuniVisionApplication.java
+++ src/main/java/com/o4/CommuniVision/CommuniVisionApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommuniVisionApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CommuniVisionApplication.class, args);
 	}
-
 }
