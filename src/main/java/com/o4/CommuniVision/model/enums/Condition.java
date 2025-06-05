e9045611441d59ecdc4b59f8f1bf9fe78ef13f45
Dray <dray.lao@gmail.com>
2025-06-05 16:47:31 +0800

    enum

---
+++ src/main/java/com/o4/CommuniVision/model/enums/Condition.java
@@ -0,0 +1,8 @@
+package com.o4.CommuniVision.model.enums;
+
+public enum Condition {
+    EXCELLENT,
+    GOOD,
+    FAIR,
+    POOR
+}
---
+++ src/main/java/com/o4/CommuniVision/model/enums/RiskLevel.java
@@ -0,0 +1,8 @@
+package com.o4.CommuniVision.model.enums;
+
+public enum RiskLevel {
+    LOW,
+    MODERATE,
+    HIGH,
+    EXTREME
+}
